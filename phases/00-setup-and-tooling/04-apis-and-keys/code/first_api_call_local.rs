fba9cc7fea9294d6f95b414875746aa8c774ad65
chrishenyard <chenyard@gmail.com>
2026-07-07 16:47:23 -0700

    Updates to rust first api

--- phases/00-setup-and-tooling/04-apis-and-keys/code/first_api_call_local.rs
+++ phases/00-setup-and-tooling/04-apis-and-keys/code/first_api_call_local.rs
@@ -13,6 +13,8 @@ struct Options {
 #[derive(Serialize, Deserialize)]
 struct MessageRequest {
     model: String,
+    max_tokens: u32,
+    stream: bool,
     messages: Vec<Message>,
     options: Options,
 }
@@ -23,22 +25,56 @@ struct Message {
     content: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "lowercase")]
+pub enum ContentItem {
+    Thinking { thinking: String },
+    Text { text: String },
+}
+
+#[derive(Deserialize, Debug, Default)]
+struct Usage {
+    #[serde(default)]
+    input_tokens: u32,
+    #[serde(default)]
+    output_tokens: u32
+}
+
+#[derive(Deserialize, Debug)]
+struct MessageResponse {
+    id: String,
+    r#type: String,
+    #[serde(default)]
+    role: String,
+    model: String,
+    #[serde(default)]
+    content: Vec<ContentItem>,
+    #[serde(default)]
+    stop_reason: String,
+    #[serde(default)]
+    usage: Usage
+}
+
 // Write function for a HTTP post request to the local API server
-async fn post_request(url: &str, message_request: &MessageRequest) -> Result<String, Box<dyn std::error::Error>> {
-    let client = reqwest::blocking::Client::new();
+async fn post_request(url: &str, message_request: &MessageRequest) -> Result<MessageResponse, Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
     let res = client.post(url)
         .header("Content-Type", "application/json")
         .body(serde_json::to_string(message_request)?)
-        .send()?;
+        .send()
+        .await?;
 
-    let response_text = res.text()?;
-    Ok(response_text)
+    let body = res.text().await?;
+    let message_response: MessageResponse = serde_json::from_str(&body)?;
+    Ok(message_response)
 }
 
 #[tokio::main]
 async fn main() -> ExitCode {
     let message_request = MessageRequest {
         model: "qwen3.5:0.8b".to_string(),
+        max_tokens: 65536,
+        stream: false,
         messages: vec![
             Message {
                 role: "user".to_string(),
@@ -54,7 +90,14 @@ async fn main() -> ExitCode {
     let response = post_request("http://localhost:11434/v1/messages", &message_request).await;
     
     match response {
-        Ok(res) => println!("Response: {}", res),
+        Ok(res) => {
+            for item in &res.content {
+                    match item {
+                        ContentItem::Thinking { thinking } => println!("AI Thought: {}", thinking),
+                        ContentItem::Text { text } => println!("AI Output: {}", text),
+                    }
+                }
+        },
         Err(err) => eprintln!("Error: {}", err),
     }
 
